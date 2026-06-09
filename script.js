ae75bc58576817a746e666f02c5a54fbe3169e23
TechDevAditya <code.raider07@gmail.com>
2026-06-09 14:21:25 +0530

    added toggle feature for nodes and wall conversion on mouse click

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const gridContainer = document.getElementById('grid');
+
 for(let r=0;r<20;r++){
     for(let c=0;c<50;c++){
         const node = document.createElement('div');
         node.className = 'node';
+
+        node.addEventListener('click', function(){   //click listener for node to turn to a wall
+
+            //to toggle bw wall mode and normal node
+            node.classList.toggle('wall');
+        });
+
         gridContainer.appendChild(node);
     }
 }
