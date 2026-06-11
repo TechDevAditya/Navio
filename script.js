b87ca5d9448d3c62d12df928d3db0e0513d8afa9
TechDevAditya <code.raider07@gmail.com>
2026-06-11 11:35:04 +0530

    added start and end node to node classList

--- script.js
+++ script.js
@@ -5,10 +5,18 @@ for(let r=0;r<20;r++){
         const node = document.createElement('div');
         node.className = 'node';
 
+        if(r===9 && c===10){
+            node.classList.add('start');
+        }else if(r===9 && c===39) {
+            node.classList.add('end');
+        }
+
         node.addEventListener('click', function(){   //click listener for node to turn to a wall
 
-            //to toggle bw wall mode and normal node
-            node.classList.toggle('wall');
+            //to toggle bw wall mode and normal node if its not start or end node
+            if(!node.classList.contains('start') && !node.classList.contains('end')){
+                node.classList.toggle('wall');
+            }
         });
 
         gridContainer.appendChild(node);
