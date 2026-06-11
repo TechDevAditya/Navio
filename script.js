2359cf56b8e209ba33ca8d3b12bdb4c7e27e703a
TechDevAditya <code.raider07@gmail.com>
2026-06-11 13:14:16 +0530

    added visual animation to change cyan nodes to yellow if the node is part of shortest path, with a delay set for the cyan wave to finish first, after writing the logic for the backtracked shortest path

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ for (let r=0;r<20;r++) {
 
 
 
-//start the algo and return data in the log
+//Master start button for dijkstra
 //NEW: added animation to the grid once the start button is clicked.
 const startDijkstra=document.getElementById('start-btn');
 
@@ -57,12 +57,19 @@ startDijkstra.addEventListener('click', function(){
     
     //run dijkstra and store the ordered array of visited nodes.
     const visitedNodes= dijkstra(grid,startNode,endNode);
+    //get the shortest path
+    const shortestPath=getShortestPath(endNode);
 
     //trigger the visual animation
     animateDijkstra(visitedNodes);
 
+    //wait for visited nodes wave to finish, then draw the yellow path
+    setTimeout(()=>{
+        animateShortestPath(shortestPath);
+    },10*visitedNodes.length);
+
     console.log("Algo finished! Nodes visited: ",visitedNodes.length);
-    console.log(visitedNodes);
+    console.log("Shortes path length: ",shortestPath.length);
 });
 
 
@@ -139,12 +146,25 @@ function dijkstra(grid,startNode,endNode){
     return visitedNodes;
 }
 
+//Backtracking logic: Work backwards from endNode till parNode!=null
+function getShortestPath(endNode){
+    const path=[];   //this is our shortest path
+    let currentNode=endNode;
+
+    while(currentNode!=null){
+        path.push(currentNode);  
+        currentNode= currentNode.parNode;
+    }
+
+    return path.reverse();
+}
+
 
 
 
 //Animation engine
 //FEATURE: creating a delay using setTimeout to queue up the animations to fire one after the other
-
+//FEATURE: Replace cyan with yellow if node is on the shortest path
 function animateDijkstra(visitedNodes){
     for(let i=0;i<visitedNodes.length;i++){
         setTimeout(()=>{
@@ -153,6 +173,25 @@ function animateDijkstra(visitedNodes){
             if(!node.isEnd && !node.isStart){
                 node.element.classList.add('node-visited'); //added the classList node-visited so it turns cyan as dictated in css
             }
-        }, 10*i) //10 ms* the index gives a sequential 10ms delay for each node in the pr queue.
+        }, 10*i); //10 ms* the index gives a sequential 10ms delay for each node in the pr queue.
     }
 }
+
+function animateShortestPath(path){  //pass the shortest path to animate
+    for(let i=0;i<path.length;i++){
+        setTimeout(()=>{
+            const node=path[i];
+
+            if(!node.isEnd && !node.isStart){
+                node.element.classList.remove('node-visited');
+                node.element.classList.add('node-shortest-path');
+            }
+        }, 50*i);
+    }
+}
+
+
+
+
+
+
