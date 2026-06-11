e138ea7fb18a381e68b4ba5cec40d8a8fd57ca66
TechDevAditya <code.raider07@gmail.com>
2026-06-11 12:22:26 +0530

    added in-memory grid in js for the dijkstra to work on

--- script.js
+++ script.js
@@ -1,24 +1,52 @@
 const gridContainer = document.getElementById('grid');
+const grid = [];  //Global array to store the in-memory graph structure
 
-for(let r=0;r<20;r++){
-    for(let c=0;c<50;c++){
-        const node = document.createElement('div');
-        node.className = 'node';
+for (let r=0;r<20;r++) {
+    const currentRow=[];
+    for (let c=0;c<50;c++) {
+        const nodeElement = document.createElement('div');
+        nodeElement.className = 'node';
 
-        if(r===9 && c===10){
-            node.classList.add('start');
-        }else if(r===9 && c===39) {
-            node.classList.add('end');
+        const isStart=(r===9 && c===10);
+        const isEnd=(r===9 && c===39);
+
+        if(isStart){
+            nodeElement.classList.add('start');
+        }else if(isEnd){
+            nodeElement.classList.add('end');
         }
 
-        node.addEventListener('click', function(){   //click listener for node to turn to a wall
+        //All the values required in dijkstra of a node
+        const nodeData = {
+            row:r,
+            col:c,
+            isStart: isStart,
+            isEnd: isEnd,
+            isWall: false,
+            isVisited: false,
+            distance: Infinity, 
+            parNode: null, 
+            element: nodeElement 
+        };
 
-            //to toggle bw wall mode and normal node if its not start or end node
-            if(!node.classList.contains('start') && !node.classList.contains('end')){
-                node.classList.toggle('wall');
+        //Wall toggling logic
+        nodeElement.addEventListener('click',function(){
+            if(!nodeData.isStart && !nodeData.isEnd){
+                nodeData.isWall = !nodeData.isWall; 
+                nodeElement.classList.toggle('wall'); //Mistake done before: Toggle the visual state as well
             }
         });
 
-        gridContainer.appendChild(node);
+        gridContainer.appendChild(nodeElement);
+        currentRow.push(nodeData); 
     }
+    
+    grid.push(currentRow);
 }
+
+//log when start button clicked to debug
+const startDijkstra=document.getElementById('start-btn');
+
+startDijkstra.addEventListener('click', function() {
+    console.log("Master Grid Data Structure:", grid);
+});
